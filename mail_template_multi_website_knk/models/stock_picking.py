c6659677bcbf6eebd96f963645f3de18b7f59cfb
niyati-mindphin <niyati@mindphin.com>
2024-03-29 15:07:23 +0530

    [IMP] Improve send mail related code when dropshipping is crated.

--- mail_template_multi_website_knk/models/stock_picking.py
+++ mail_template_multi_website_knk/models/stock_picking.py
@@ -10,7 +10,7 @@ class StockPicking(models.Model):
 
     def _send_confirmation_email(self):
         if self._context.get('is_mail_send'):
-            for stock_pick in self.filtered(lambda p: p.website_id.delivery_confirmation_mail_template and p.picking_type_id.code == 'outgoing'):
+            for stock_pick in self.filtered(lambda p: p.website_id.delivery_confirmation_mail_template and p.picking_type_id.code == 'outgoing' or p.picking_type_id.code == 'incoming'):
                 delivery_template_id = stock_pick.website_id.delivery_confirmation_mail_template.id
                 stock_pick.with_context(force_send=True).message_post_with_template(delivery_template_id, email_layout_xmlid='mail.mail_notification_light')
 
