85340b7e3934e40fbdb18520da88c19eed0dbe35
niyati-mindphin <niyati@mindphin.com>
2024-05-23 09:44:57 +0530

    bkp module md_print_survey_and_mail

---
+++ md_print_survey_and_mail/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Powered by Mindphin.
+# © 2023 Mindphin. (<https://www.mindphin.com>).
---
+++ md_print_survey_and_mail/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Powered by Mindphin.
+# © 2023 Mindphin. (<https://www.mindphin.com>).
+
+{
+    'name': 'Print Survey and Mail',
+    'summary': """
+    """,
+    'description': """ """,
+    'author': 'Mindphin',
+    'website': 'https://www.mindphin.com',
+    'category': 'Survey',
+    'version': '17.0.1.0',
+    'license': 'OPL-1',
+    'depends': ["survey"],
+    'data': [
+        'data/survey_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'price': 10,
+    'currency': "USD",
+}
